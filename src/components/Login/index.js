9d75cacc943ab0c0b0cd8089b6c412d4b2d31382
mahendra5maddipati <mahendra5maddipati@gmail.com>
2025-04-08 01:49:25 +0000

    need to style login page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,4 +1,6 @@
 import react, {useState} from 'react'
+import {useHistory} from 'react-router-dom'
+import Cookies from 'js-cookie'
 
 import './index.css'
 
@@ -7,6 +9,37 @@ const Login = () => {
   const [password, setPassword] = useState('')
   const [showSubmitError, setShowSubmitError] = useState(false)
   const [errorMsg, setErrorMsg] = useState('')
+  const history = useHistory()
+
+  const onSubmitSuccess = jwtToken => {
+    // const {history} = props
+    Cookies.set('jwt_token', jwtToken, {
+      expires: 5,
+    })
+    history.replace('/')
+  }
+
+  const onSubmitFailure = msg => {
+    setShowSubmitError(true)
+    setErrorMsg(msg)
+  }
+
+  const submitForm = async event => {
+    event.preventDefault()
+    const userDetails = {username, password}
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      onSubmitSuccess(data.jwt_token)
+    } else {
+      onSubmitFailure(data.error_msg)
+    }
+  }
 
   return (
     <div className="login-container">
@@ -15,14 +48,38 @@ const Login = () => {
         className="login-img"
         alt="login"
       />
-      <form className="form-container">
+      <form className="form-container" onSubmit={submitForm}>
         <img
           src="https://res.cloudinary.com/dq6xf1yud/image/upload/v1744073435/logo_d9z1zi.png"
           className="website-logo"
           alt="website logo"
         />
-        <div className="input-container">{}</div>
-        <div className="input-container">{}</div>
+        <div className="input-container">
+          <label className="input-label" htmlFor="username">
+            Password
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="username-field"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            placeholder="rahul"
+          />
+        </div>
+        <div className="input-container">
+          <label className="input-label" htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="password-field"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            placeholder="rahul@2021"
+          />
+        </div>
         <button type="submit" className="login-botton">
           Login
         </button>
